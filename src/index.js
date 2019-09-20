fb44ea7ebf033d07b9a25da97b6cb7f4fbd7452e
Juliana Fogg <julianafogg@gmail.com>
2019-09-20 09:37:18 -0400

    add fetchCategories

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import categoryReducer  from './reducers/categoryReducer';
 
 
-const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
-let store =createStore(reducer,composeEnhancers(applyMiddleware(thunk)))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(categoryReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-<Provider store={store}>
-  <App />
-</Provider>, 
-document.getElementById('root')
-);
+  <Provider store={store}>
+  
+      <App />
+  
+  </Provider>,
+  document.getElementById('root'));
 
