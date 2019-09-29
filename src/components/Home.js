45b48f4fb7f164ed57cf808b9888e767803da95e
Juliana Fogg <julianafogg@gmail.com>
2019-09-29 12:19:53 -0400

    add home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Be peaceful, kind and accepting.</h1>
-      <quote>“For children, mindfulness can offer relief from whatever difficulties they might be encountering in life.” “It also gives them the beauty of being in the present moment.” -Annaka Harris></quote>
+      <h3>“For children, mindfulness can offer relief from whatever difficulties they might be encountering in life.” “It also gives them the beauty of being in the present moment.” -Annaka Harris</h3>
     </div>
   );
 };
